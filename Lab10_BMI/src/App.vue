523f970fa5a8c7d392b273651737b2fa65f00e5d
Jdono18 <156264657+Jdono18@users.noreply.github.com>
2024-03-31 14:37:54 -0500

    Initial commit

---
+++ Lab10_BMI/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import { ref } from 'vue'
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+
+const bmi = ref(0)
+const calculateBMI = (height, weight) => {
+  const bmi = weight / (height * height)
+  alert(`todo - do math! height is ${height} , ${weight} your BMI is ${bmi}`)
+}
+
+</script>
+
+<template>
+  <!-- here's the HTML for the component -->
+  <div id="app-component">
+    <h1> Body Mass Index Calculator</h1>
+
+
+  </div>
+
+  <BodyMassIndexForm
+      v-on:stats-entered="calculateBMI"
+      ></BodyMassIndexForm>
+
+  <p> Your BMI is: {{ bmi.value }}  </p>
+
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ Lab10_BMI/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  // extra question needs this, the basic app does not
+
+})
+
+// define the message that this component may send to other components
+const emit = defineEmits(['stats-entered'])
+
+// we can pick the above names - this message will be sent once the user has entered their data and the app should do the math
+
+const height = ref('0')
+const weight = ref('0')
+
+function statsEntered() {
+  alert('is this working?')
+  // todo send this data - height and weight to App.vue to do the math
+  // make sure to include weight
+
+  emit('stats-entered', height.value, weight.value) // goes to this components parent
+}
+</script>
+
+<template>
+  <label> Height</label>
+  <input v-model="height">
+
+  <label> Weight</label>
+  <input v-model="weight"> <br> <br>
+
+  <button v-on:click="statsEntered"> Calculate </button>
+
+
+
+</template>
+
+
+<style scoped>
+button{
+  font-size: 17px;
+  padding: 15px;
+  background-color: mediumpurple;
+  border-radius: 10px;
+  color: white;
+}
+
+</style>
